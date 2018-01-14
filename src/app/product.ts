372c3b0acc7dc28da4ae73d006b28298deb57a03
Christian Zellweger <chzellweger@gmail.com>
2018-01-14 21:07:25 +0100

    additional fields for Product model

--- src/app/product.ts
+++ src/app/product.ts
@@ -2,9 +2,12 @@ export class Product {
     id: number;
     name: string;
     description: string;
+    descriptionLong: string;
+    price: number;
+    reduction?: number;
     imageURLs: string[];
-    ratingIDs: number[];
-    reviewIDs: number[];
+    ratingIDs?: number[];
+    reviewIDs?: number[];
     sizes: number[];
     colors: string[];
     categoryIDs: number[];
