506ce1feba086ba8e9ef872956cfd7e8c9962423
André Abt <info@andre-abt.com>
2018-01-30 16:36:15 +0100

    Added more cart.sercive basics

--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,7 +1,9 @@
+import { EventEmitter } from '@angular/core';
 import { Product } from '../products/shared/product.model';
 
 export class CartService {
   private cartItems: Product[] = [];
+  public itemsChanged: EventEmitter<Product[]> = new EventEmitter<Product[]>();
 
   constructor() { }
 
@@ -11,10 +13,17 @@ export class CartService {
 
   addItem(product: Product) {
     this.cartItems.push(product);
+    this.itemsChanged.emit(this.cartItems.slice());
+  }
+
+  addItems(products: Product[]) {
+    this.cartItems.push(...products);
+    this.itemsChanged.emit(this.cartItems.slice());
   }
 
   clearCart() {
     this.cartItems = [];
+    this.itemsChanged.emit(this.cartItems.slice());
   }
 
 }
