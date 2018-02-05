bf1e5836f5138a51f9af616839a4502cd2b125f9
André Abt <info@andre-abt.com>
2018-02-05 16:31:38 +0100

    Different test images

--- src/app/model/in-memory-data.service.ts
+++ src/app/model/in-memory-data.service.ts
@@ -13,7 +13,7 @@ export class InMemoryDataService implements InMemoryDbService {
         reduction: 70,
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
         descriptionLong: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.',
-        imageURLs: ['img/shop/products/01.jpg', 'img/shop/products/01.jpg'],
+        imageURLs: ['img/shop/products/01.jpg', 'img/shop/products/02.jpg'],
         ratingIDs: [1],
         reviewIDs: [],
         sizes: [9, 10, 11] ,
