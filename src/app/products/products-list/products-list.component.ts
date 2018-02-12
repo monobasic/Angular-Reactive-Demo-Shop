ebe30c176748f48a42dd08497d2a36c18cfec20d
André Abt <info@andre-abt.com>
2018-02-12 14:24:50 +0100

    Added pagination logic

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -4,6 +4,7 @@ import { DecimalPipe } from '@angular/common';
 import { Product } from '../shared/product.model';
 
 import { ProductService } from '../shared/product.service';
+import { PagerService } from '../../core/pager/pager.service';
 
 @Component({
   selector: 'app-products',
@@ -12,9 +13,11 @@ import { ProductService } from '../shared/product.service';
 })
 export class ProductsListComponent implements OnInit {
   products: Product[];
+  productsPaged: Product[];
   displayMode: string;
+  pager: any = {};
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private pagerService: PagerService) { }
 
   ngOnInit() {
     this.displayMode = 'grid';
@@ -25,11 +28,20 @@ export class ProductsListComponent implements OnInit {
     this.productService.getProducts()
       .subscribe(products => {
         this.products = products;
+        this.setPage(1);
       });
   }
 
   onDisplayModeChange(mode: string, e: Event) {
     this.displayMode = mode;
     e.preventDefault();
+  }
+
+  setPage(page: number) {
+    if (page < 1 || page > this.pager.totalPages) {
+      return;
+    }
+    this.pager = this.pagerService.getPager(this.products.length, page, 4);
+    this.productsPaged = this.products.slice(this.pager.startIndex, this.pager.endIndex + 1);
   }
 }
