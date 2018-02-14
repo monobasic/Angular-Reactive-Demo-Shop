2cd94bf3f6cde2bc7a2b522e3caa08b4cff305d5
André Abt <info@andre-abt.com>
2018-02-14 14:59:03 +0100

    Moved stuff

--- src/app/checkout/address/address.component.ts
+++ src/app/checkout/address/address.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout-address',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address.component.scss']
 })
 export class AddressComponent implements OnInit {
+  formAddress: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    // Init Form
+    this.formAddress = new FormGroup({
+      'firstname': new FormControl(null, Validators.required),
+      'lastname': new FormControl(null, Validators.required),
+      'address1': new FormControl(null, Validators.required),
+      'address2': new FormControl(null),
+      'zip': new FormControl(null, [Validators.required, Validators.pattern(/^\d\d\d\d$/)]),
+      'city': new FormControl(null, Validators.required),
+    });
+  }
+
+  onSubmit() {
+    console.log('form submit!');
+    console.log(this.formAddress.value);
   }
 
 }
--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { CartService } from '../cart/cart.service';
-import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -11,7 +10,6 @@ export class CheckoutComponent implements OnInit {
   cartSubtotal: number;
   shipping: number;
   orderTotal: number;
-  formAddress: FormGroup;
   activeStep: number;
   steps: string[];
 
@@ -25,21 +23,9 @@ export class CheckoutComponent implements OnInit {
     this.steps = ['1. Address', '2. Shipping', '3. Payment', '4. Review'];
     this.activeStep = 0;
 
-    // Init Form
-    this.formAddress = new FormGroup({
-      'firstname': new FormControl(null, Validators.required),
-      'lastname': new FormControl(null, Validators.required),
-      'address1': new FormControl(null, Validators.required),
-      'address2': new FormControl(null),
-      'zip': new FormControl(null, [Validators.required, Validators.pattern(/^\d\d\d\d$/)]),
-      'city': new FormControl(null, Validators.required),
-    });
+
   }
 
-  onSubmit() {
-    console.log('form submit!');
-    console.log(this.formAddress.value);
-  }
 
   onStepClicked(event: Event, index: number) {
     this.activeStep = index;
