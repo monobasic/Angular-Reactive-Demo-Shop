dfe8c48492586a7f3926fe03bcc32026632086dc
André Abt <info@andre-abt.com>
2018-02-14 16:28:30 +0100

    Added customer model

---
+++ src/app/model/customer.model.ts
@@ -0,0 +1,12 @@
+export class Customer {
+    firstname: string;
+    lastname: string;
+    address1: string;
+    address2: string;
+    zip: number;
+    city: string;
+    email: string;
+    phone: string;
+    company: string;
+    country: string;
+}
