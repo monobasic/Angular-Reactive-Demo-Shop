466225a441811b163c4cb86fb0b059e3fbf207f6
Christian Zellweger <chzellweger@gmail.com>
2018-02-18 21:22:50 +0100

    refactor product routes

--- server/ts-source/routes/products.routes.ts
+++ server/ts-source/routes/products.routes.ts
@@ -1,32 +1,39 @@
-import productsController from '../controller/products.controller';
+import {
+  getProducts,
+  getProduct,
+  addProduct,
+  updateProduct,
+  uploadImages,
+  resizeImages,
+  createProduct,
+  deleteProduct,
+  log
+} from '../controller/products.controller';
 import ProductModel from '../model/product.model';
 
 import * as express from 'express';
 import { Router } from 'express';
 import { catchErrors } from '../errorHandlers';
-import { createProduct } from '../controller/products.controller';
 
 const router = Router();
 
-router.get('/', productsController.getProducts);
-router.post(
-  '/',
-  // productsController.log,
-  productsController.uploadImages,
-  productsController.log,
-  catchErrors(productsController.resizeImages),
-  productsController.createProduct,
-  catchErrors(productsController.addProduct)
+router.get('/', getProducts);
+router.post('/',
+  catchErrors(uploadImages),
+  catchErrors(log),
+  catchErrors(resizeImages),
+  catchErrors(createProduct),
+  catchErrors(addProduct)
 );
 
-router.put(
-  '/:id',
-  productsController.uploadImages,
-  productsController.log,
-  catchErrors(productsController.resizeImages),
-  catchErrors(productsController.updateProduct)
+router.get('/:id', catchErrors(getProduct));
+router.delete('/:id', catchErrors(deleteProduct));
+router.put('/:id',
+  catchErrors(uploadImages),
+  catchErrors(log),
+  catchErrors(resizeImages),
+  catchErrors(updateProduct)
 );
-router.get('/:id', catchErrors(productsController.getProduct));
-router.delete('/:id', catchErrors(productsController.deleteProduct));
+
 
 export default router;
