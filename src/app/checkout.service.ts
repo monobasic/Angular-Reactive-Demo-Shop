a30cd28a67733a582da20cb7326a0c67dabbd591
André Abt <info@andre-abt.com>
2018-02-20 15:29:44 +0100

    Refactored logic to service

--- src/app/checkout.service.ts
+++ src/app/checkout.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Order } from './model/order.model';
 
 @Injectable()
 export class CheckoutService {
+  orderInProgress: Order;
+  stepChanged: EventEmitter<number> = new EventEmitter<number>();
+  activeStep: number;
 
-  constructor() { }
+  constructor() {
+    this.orderInProgress = new Order();
+    this.activeStep = 0;
+  }
 
+  changeStep(number) {
+    this.activeStep = number;
+    this.stepChanged.emit(this.activeStep);
+  }
 }
--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,10 +1,5 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import { Order } from '../model/order.model';
-import { OrderService } from '../order.service';
-import { AddressComponent } from './address/address.component';
-import { Customer } from '../model/customer.model';
-import { ShippingComponent } from './shipping/shipping.component';
+import { Component, OnInit } from '@angular/core';
+import { CheckoutService } from '../checkout.service';
 
 @Component({
   selector: 'app-checkout',
@@ -12,94 +7,19 @@ import { ShippingComponent } from './shipping/shipping.component';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
-  activeStep: number;
   steps: string[];
-  orderInProgress: Order;
-  stepsValidated: number[];
+  activeStep: number;
 
-  @ViewChild(AddressComponent)
-  private addressComponent: AddressComponent;
-
-  @ViewChild(ShippingComponent)
-  private shippingComponent: ShippingComponent;
-
-  constructor(private orderService: OrderService) { }
+  constructor(private checkoutService: CheckoutService) { }
 
   ngOnInit() {
     this.steps = ['1. Address', '2. Shipping', '3. Payment', '4. Review'];
-    this.activeStep = 0;
-    this.stepsValidated = [];
-    this.orderInProgress = new Order();
-  }
-
-  // onStepClicked(event: Event, index: number) {
-  //   this.activeStep = index;
-  //   event.preventDefault();
-  // }
-
-  onStepChanged(step: number) {
-    // Validate different steps
-    if (step === 1) {
-      console.log('validate step 0 (= 1. Address)');
-      if (this.addressComponent.formAddress.valid) {
-
-        // Save form data to orderInProgress
-        this.orderInProgress.customer = new Customer();
-        this.orderInProgress.customer = this.addressComponent.formAddress.value;
-
-        // Set step statue
-        if (!this.stepsValidated.includes(step - 1)) {
-          this.stepsValidated.push(step - 1);
-        }
-
-        // Change step
+    this.activeStep = this.checkoutService.activeStep;
+    this.checkoutService.stepChanged.subscribe(
+      (step: number) => {
         this.activeStep = step;
-      } else {
-        this.validateAllFields(this.addressComponent.formAddress);
       }
-    } else if (step === 2) {
-      console.log('validate step 1 (= 2. Shipping)');
-      console.log('validate step 0 (= 1. Address)');
-      if (this.shippingComponent.formShipping.valid) {
-
-        // Save form data to orderInProgress
-        this.orderInProgress.shippingMethod = this.shippingComponent.formShipping.controls.shippingMethod.value;
-
-        // Set step statue
-        if (!this.stepsValidated.includes(step - 1)) {
-          this.stepsValidated.push(step - 1);
-        }
-
-        // Change step
-        this.activeStep = step;
-      } else {
-        this.validateAllFields(this.shippingComponent.formShipping);
-      }
-
-    } else if (step === 3) {
-      console.log('validate step 2 (= 3. Payment)');
-    } else {
-      this.activeStep = step;
-    }
-
-    console.log(this.orderInProgress);
-
-  }
-
-  onCompleteOrder(event: Event) {
-    console.log('complete order!');
-    this.orderService.addOrder(this.orderInProgress);
-  }
-
-  validateAllFields(formGroup: FormGroup) {
-    Object.keys(formGroup.controls).forEach(field => {
-      const control = formGroup.get(field);
-      if (control instanceof FormControl) {
-        control.markAsTouched({ onlySelf: true });
-      } else if (control instanceof FormGroup) {
-        this.validateAllFields(control);
-      }
-    });
+    );
   }
 
 }
