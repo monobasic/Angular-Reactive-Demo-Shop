d4e750f9ba5db58254bd5e1d42de6c230f3c9384
André Abt <info@andre-abt.com>
2018-02-20 13:03:11 +0100

    Added shipping form and handling

--- src/app/checkout/shipping/shipping.component.ts
+++ src/app/checkout/shipping/shipping.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout-shipping',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shipping.component.scss']
 })
 export class ShippingComponent implements OnInit {
+  formShipping: FormGroup;
+  shippingMethods: {method: string, time: string, fee: number, value: string}[];
 
   constructor() { }
 
   ngOnInit() {
+    this.shippingMethods = [
+      {
+        method: 'Swiss Post Priority',
+        time: '1 - 2 days',
+        fee: 11,
+        value: 'priority'
+      },
+      {
+        method: 'Swiss Post Economy',
+        time: 'up to one week',
+        fee: 9,
+        value: 'economy'
+      }
+    ]
+    this.formShipping = new FormGroup({
+      'shippingMethod': new FormControl(this.shippingMethods[1].value, Validators.required)
+    });
   }
 
 }
