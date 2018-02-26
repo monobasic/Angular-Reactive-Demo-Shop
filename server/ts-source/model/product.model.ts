9344e8fa2e8dc4814f8cacfd93341d1b6e0013d3
Christian Zellweger <chzellweger@gmail.com>
2018-02-26 15:09:01 +0100

    add timestamp to models

--- server/ts-source/model/product.model.ts
+++ server/ts-source/model/product.model.ts
@@ -1,16 +1,19 @@
 import * as mongoose from 'mongoose';
 import * as mongoosePaginate from 'mongoose-paginate';
 
-const productsSchema = new mongoose.Schema({
-  id: { type: Number},
-  name: { type: String },
-  description: { type: String },
-  price: { type: Number },
-  priceNormal: { type: String },
-  reduction: { type: Number},
-  imageURLs: { type: Array},
-  categories: Array
-});
+const productsSchema = new mongoose.Schema(
+  {
+    id: { type: Number },
+    name: { type: String },
+    description: { type: String },
+    price: { type: Number },
+    priceNormal: { type: String },
+    reduction: { type: Number },
+    imageURLs: { type: Array },
+    categories: Array
+  },
+  { timestamps: true }
+);
 
 productsSchema.plugin(mongoosePaginate);
 
