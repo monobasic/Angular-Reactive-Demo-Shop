3d852ec89062c22ea219bdaef518085f82e3bb5f
André Abt <info@andre-abt.com>
2018-03-12 16:22:13 +0100

    Removed server part

--- server/ts-source/config/database.ts
+++
@@ -1,9 +0,0 @@
-const user = process.env.DB_USER;
-const pw = process.env.DB_PASS;
-const host = process.env.DB_HOST;
-
-const database = {
-  database: `mongodb://${user}:${pw}@${host}`
-};
-
-export default database;
