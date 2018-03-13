934700b29e68a1ece70b1160f2e21de1411e1b4a
André Abt <info@andre-abt.com>
2018-03-13 13:20:15 +0100

    Refactored admin.guard

--- src/app/admin/shared/admin.guard.ts
+++ src/app/admin/shared/admin.guard.ts
@@ -2,32 +2,32 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { MessageService } from '../../messages/message.service';
-import { AdminAuthService } from './admin-auth.service';
-import { createProduct } from '../../../../server/ts-source/controller/products.controller';
-import { UserService } from '../../account/shared/user.service';
+import { AuthService } from '../../account/shared/auth.service';
+
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
   constructor(
     private log: MessageService,
-    private adminAuthService: AdminAuthService,
-    private authenticationService: UserService
+    private authService: AuthService
   ) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    this.log.add('Authenticated');
 
-    return Observable.create((obs) => {
-      const auth = this.authenticationService.adminAuth()
-      .subscribe((answer) => {
-        if (answer.auth) {
-          obs.next(true);
-        } else {
-          obs.next(false);
+
+    return this.authService.user
+      .take(1)
+      .map(user => user.roles.admin ? user.roles.admin : false)
+      .do(authorized => {
+        if (!authorized) {
+          console.log('route prevented!');
         }
       });
-    });
+
   }
 }
