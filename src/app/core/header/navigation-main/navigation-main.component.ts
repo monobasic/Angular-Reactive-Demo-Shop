3d8018bd0cd799b8067593e733d662c121afc91f
André Abt <info@andre-abt.com>
2018-03-13 13:20:36 +0100

    Added simplified admin check

--- src/app/core/header/navigation-main/navigation-main.component.ts
+++ src/app/core/header/navigation-main/navigation-main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { User } from '../../../models/user.model';
+import { AuthService } from '../../../account/shared/auth.service';
 
 @Component({
     selector: 'app-navigation-main',
@@ -6,8 +9,11 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./navigation-main.component.scss']
 })
 export class NavigationMainComponent implements OnInit {
+    user: BehaviorSubject<User>;
 
-    constructor() {}
+    constructor(public authService: AuthService) {}
 
-    ngOnInit() {}
+  ngOnInit() {
+    this.user = this.authService.user;
+  }
 }
