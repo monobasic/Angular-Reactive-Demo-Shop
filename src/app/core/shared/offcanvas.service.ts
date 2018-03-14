9e991931a2ba3413ba859f57b50df2521c39c7df
André Abt <info@andre-abt.com>
2018-03-14 15:31:15 +0100

    Added methods

--- src/app/core/shared/offcanvas.service.ts
+++ src/app/core/shared/offcanvas.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class OffcanvasService {
+  offcanvasNavigationOpen: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
-  constructor() { }
+  constructor() {
+  }
+
+  toggleOffcanvasNavigation() {
+    const state = !this.offcanvasNavigationOpen.getValue();
+    this.offcanvasNavigationOpen.next(state);
+    console.log('toggleOffcanvasNavigation() called with: ' + state);
+  }
+
+  openOffcanvasNavigation() {
+    this.offcanvasNavigationOpen.next(true);
+  }
+
+  closeOffcanvasNavigation() {
+    this.offcanvasNavigationOpen.next(false);
+  }
 
 }
