0e9729debacbf730b4d9775b7c2978ff16cee12f
André Abt <info@andre-abt.com>
2018-03-14 16:55:51 +0100

    Added mobile navigation stuff

--- src/app/core/navigation-off-canvas/navigation-off-canvas.component.ts
+++ src/app/core/navigation-off-canvas/navigation-off-canvas.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OffcanvasService } from '../shared/offcanvas.service';
+import { AuthService } from '../../account/shared/auth.service';
+import { Router } from '@angular/router';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-navigation-off-canvas',
@@ -7,10 +10,22 @@ import { OffcanvasService } from '../shared/offcanvas.service';
   styleUrls: ['./navigation-off-canvas.component.scss']
 })
 export class NavigationOffCanvasComponent implements OnInit {
+  user: User;
 
-  constructor(public offcanvasService: OffcanvasService) { }
+  constructor(public offcanvasService: OffcanvasService,
+    public authService: AuthService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.authService.user.subscribe(user => {
+      this.user = user;
+    });
   }
 
+
+  onLogOut(e: Event) {
+    this.authService.signOut();
+    this.router.navigate(['/register-login']);
+    e.preventDefault();
+  }
 }
