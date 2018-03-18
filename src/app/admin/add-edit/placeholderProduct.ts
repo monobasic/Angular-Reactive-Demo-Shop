bd64d986fd84ec1d443db1c676c5000abab0a0a9
Christian Zellweger <chzellweger@gmail.com>
2018-03-18 16:09:31 +0100

    disable submit on invalid form

--- src/app/admin/add-edit/placeholderProduct.ts
+++ src/app/admin/add-edit/placeholderProduct.ts
@@ -3,8 +3,8 @@ import { Product } from '../../models/product.model';
 export const placeholderProduct: Product = {
   id: null,
   date: new Date().toString(),
-  name: 'Placeholder Product Name',
-  description: 'Come up with something descriptive',
+  name: '',
+  description: '',
   price: 1000,
   priceNormal: 2000,
   imageURLs: ['img/shop/products/13.jpg'],
