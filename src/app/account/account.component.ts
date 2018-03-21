d24c610ffa95dcb481a55ba6389f952e6b7fdbf2
André Abt <info@andre-abt.com>
2018-03-21 14:20:02 +0100

    clean up

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  ActivatedRoute,
-  UrlSegment,
-  ActivatedRouteSnapshot
-} from '@angular/router';
-import { Location } from '@angular/common';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { User } from '../models/user.model';
 import { AuthService } from './shared/auth.service';
@@ -16,23 +10,10 @@ import { AuthService } from './shared/auth.service';
 })
 export class AccountComponent implements OnInit {
   user: User;
-  currentLocation;
 
-  constructor(private route: ActivatedRoute,
-    private location: Location,
-    private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    console.log(this.route.snapshot);
-    console.log(this.route.children);
-    const loc = this.route.params.subscribe((para) => {
-      console.log(para);
-    });
-
-
     this.authService.user.subscribe(user => this.user = user);
-    console.log(this.user);
-
   }
-  getLocation() {}
 }
