87fe86d76a5eb4309a9401c08fce204cf64cc158
André Abt <info@andre-abt.com>
2018-03-21 12:51:51 +0100

    Added promo

--- src/app/core/home/promo/promo.component.ts
+++ src/app/core/home/promo/promo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Promo } from '../../../models/promo.model';
 
 @Component({
   selector: 'app-promo',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./promo.component.scss']
 })
 export class PromoComponent implements OnInit {
+  @Input()
+  promo: Promo;
 
   constructor() { }
 
