3cd2ba26f87550a4d18d91275b03c42f09128f6a
Christian Zellweger <chzellweger@gmail.com>
2018-03-22 16:14:42 +0100

    add date and sale-props to placeholder-product

--- src/app/admin/add-edit/placeholderProduct.ts
+++ src/app/admin/add-edit/placeholderProduct.ts
@@ -2,12 +2,13 @@ import { Product } from '../../models/product.model';
 
 export const placeholderProduct: Product = {
   id: null,
-  date: new Date().toString(),
+  date: new Date().toISOString().split('T')[0],
   name: '',
   description: '',
   price: 1000,
   priceNormal: 2000,
   imageURLs: [],
   categories: ['Some', 'Example', 'Categories'],
-  reduction: 50
+  reduction: 50,
+  sale: true
 };
