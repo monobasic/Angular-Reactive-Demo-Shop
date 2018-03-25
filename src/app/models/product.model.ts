7d0d259e6a1b541f437fc20ac980d941bc0bd503
André Abt <info@andre-abt.com>
2018-03-25 10:27:44 +0200

    Updated product model

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,15 +1,17 @@
 export class Product {
-    id: number;
-    date?: string;
-    name: string;
-    description: string;
-    price: number;
-    priceNormal: number;
-    reduction?: number;
-    imageURLs: string[];
-    imageRefs?: string[];
-    categories: string[];
-    ratings?: {};
-    currentRating?: number;
-    sale?: boolean;
+    constructor(
+        public id: number,
+        public date: string,
+        public name: string,
+        public description: string,
+        public price: number,
+        public priceNormal: number,
+        public reduction: number,
+        public imageURLs: string[],
+        public imageRefs: string[],
+        public categories: string[],
+        public ratings: {},
+        public currentRating: number,
+        public sale: boolean
+    ) {}
 }
