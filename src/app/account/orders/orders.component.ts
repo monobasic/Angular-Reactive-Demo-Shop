5929690a0e66217783cf76c7c7495ef45a3dc197
Christian Zellweger <chzellweger@gmail.com>
2018-03-27 20:11:56 +0200

    refactor order-service

--- src/app/account/orders/orders.component.ts
+++ src/app/account/orders/orders.component.ts
@@ -9,10 +9,9 @@ import {
 import { Order } from '../../models/order.model';
 import { Observable } from 'rxjs/Observable';
 import { from } from 'rxjs/observable/from';
-import { mergeMap } from 'rxjs/operator/mergeMap';
-import { concatMap, toArray, tap } from 'rxjs/operators';
+import { concatMap, tap, flatMap } from 'rxjs/operators';
 import { of } from 'rxjs/observable/of';
-import { map } from 'rxjs/operator/map';
+import { mergeMap } from 'rxjs/operator/mergeMap';
 
 @Component({
   selector: 'app-orders',
@@ -21,18 +20,13 @@ import { map } from 'rxjs/operator/map';
 })
 export class OrdersComponent implements OnInit {
   public orders$: Observable<any>;
-  public orderss$: Observable<any>;
 
   constructor(public orderService: OrderService) {}
 
   ngOnInit() {
     this.orderService.getOrders();
-
-    this.orderService.orders$.map((orders) => {
-      const ordersArray = Object.values(orders.value);
-      return of(ordersArray);
-    }).subscribe((ordersArray$) => {
-      this.orders$ = ordersArray$;
+    this.orderService.orders$.subscribe(orders => {
+      this.orders$ = orders;
     });
   }
 }
--- src/app/account/orders/shared/order.service.ts
+++ src/app/account/orders/shared/order.service.ts
@@ -1,22 +1,29 @@
-import { Injectable, EventEmitter, OnInit } from '@angular/core';
-import { Order } from '../../../models/order.model';
-import { MessageService } from '../../../messages/message.service';
-import { AuthService } from '../../shared/auth.service';
-import { AngularFireDatabase } from 'angularfire2/database';
-import { tap, take, pluck, flatMap } from 'rxjs/operators';
-import { User } from '../../../models/user.model';
-import { of } from 'rxjs/observable/of';
-import { Observable } from 'rxjs/Observable';
+import { Injectable, OnInit } from '@angular/core';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import { tap } from 'rxjs/operators/tap';
+import { of } from 'rxjs/observable/of';
+import { map } from 'rxjs/operator/map';
 import { fromPromise } from 'rxjs/observable/fromPromise';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+
+import { Order } from '../../../models/order.model';
+import { User } from '../../../models/user.model';
+
+import { MessageService } from '../../../messages/message.service';
+import { AuthService } from '../../shared/auth.service';
+
 @Injectable()
-export class OrderService implements OnInit {
-  private orders: Order[];
-  private privateOrders$: Subject<Observable<any>> = new Subject();
+export class OrderService {
+  private privateOrders$: BehaviorSubject<
+    Observable<any>
+  > = new BehaviorSubject(of(null));
   public orders$ = this.privateOrders$.asObservable();
-  private userId;
-  public ordersChanged: EventEmitter<Order[]> = new EventEmitter<Order[]>();
 
   constructor(
     private messageService: MessageService,
@@ -24,48 +31,21 @@ export class OrderService implements OnInit {
     private store: AngularFireDatabase
   ) {}
 
-  ngOnInit() {
-    this.userId = this.authService.getUserUid();
-  }
+  public getOrders() {
+    this.authService.userUid$
+      .pipe(
+        tap((userUid) => {
+          if (userUid) {
+            const remoteUserOrders = `/users/${userUid}/orders`;
+            const dbOperation = this.store
+              .list(remoteUserOrders)
+              .valueChanges();
 
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.messageService.addError(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
-
-  public getOrders(): void {
-    this.authService.userUid$.subscribe((userUid) => {
-      if (userUid) {
-        const remoteUser = `/users/${userUid}`;
-        this.store
-          .object<User>(remoteUser)
-          .valueChanges()
-          .pipe(pluck('orders'))
-          .subscribe((orders) => {
-            this.privateOrders$.next(of(orders));
-          });
-      }
-    });
-  }
-
-  public getOrder(number: number) {
-    return this.orders.filter((order) => order.number === number.toString());
-  }
-
-  private constructOrderMetaData(order: Order) {
-    return {
-      number: (Math.random() * 10000000000).toString().split('.')[0],
-      date: new Date().toString(),
-      status: 'In Progress'
-    };
+            this.privateOrders$.next(dbOperation);
+          }
+        })
+      )
+      .subscribe((val) => console.log(val), (error) => console.log(error));
   }
 
   public addUserOrder(order: Order, total: number, user: string) {
@@ -96,5 +76,26 @@ export class OrderService implements OnInit {
       .then((response) => response, (error) => error);
 
     return fromPromise(databaseOperation);
+  }
+
+  private constructOrderMetaData(order: Order) {
+    return {
+      number: (Math.random() * 10000000000).toString().split('.')[0],
+      date: new Date().toString(),
+      status: 'In Progress'
+    };
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.messageService.addError(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
