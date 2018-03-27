2c7a50d006e168a21bb748c1f50d7ba3450b383b
André Abt <info@andre-abt.com>
2018-03-27 14:29:49 +0200

    Make TS linter happier

--- src/app/core/page-title/page-title.component.ts
+++ src/app/core/page-title/page-title.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PageTitleComponent implements OnInit {
   @Input() title: string;
-  @Input() children: {}[];
+  @Input() children: {title: string, link: string}[];
 
   constructor() { }
 
