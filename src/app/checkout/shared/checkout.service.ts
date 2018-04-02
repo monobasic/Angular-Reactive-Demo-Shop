8603064b4a45e28b489468ffc43e3a4658065f7e
Christian Zellweger <chzellweger@gmail.com>
2018-04-02 13:52:58 +0200

    fix step-bug on multiple orders

--- src/app/checkout/shared/checkout.service.ts
+++ src/app/checkout/shared/checkout.service.ts
@@ -36,6 +36,10 @@ export class CheckoutService {
     console.log(this.orderInProgress);
   }
 
+  resetSteps() {
+    this.activeStep = 0;
+  }
+
   setCustomer(customer: Customer) {
     this.orderInProgress.customer = customer;
     this.orderInProgressChanged.emit(this.orderInProgress);
