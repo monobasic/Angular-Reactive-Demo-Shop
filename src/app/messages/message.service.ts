646f1060dc8c154c21e209ea4e9a26b24d93360c
André Abt <info@andre-abt.com>
2018-04-03 20:38:55 +0200

    Refactored config

--- src/app/messages/message.service.ts
+++ src/app/messages/message.service.ts
@@ -4,23 +4,20 @@ import { ToastrService } from 'ngx-toastr';
 @Injectable()
 export class MessageService {
   messages: string[] = [];
+  toastrConfig: {} = {
+    disableTimeOut: false,
+    closeButton: false,
+    positionClass: 'toast-bottom-right'
+  };
 
   constructor(private toastr: ToastrService) {}
 
   add(message: string): void {
     this.messages.push(message);
-    this.toastr.success(message, 'Message:', {
-      disableTimeOut: false,
-      closeButton: false,
-      positionClass: 'toast-bottom-right'
-    });
+    this.toastr.success(message, 'Message:', this.toastrConfig);
   }
   addError(message: string): void {
-    this.toastr.error(message, 'Message:', {
-      disableTimeOut: false,
-      closeButton: false,
-      positionClass: 'toast-bottom-right'
-    });
+    this.toastr.error(message, 'Message:', this.toastrConfig);
   }
 
   clear(): void {
