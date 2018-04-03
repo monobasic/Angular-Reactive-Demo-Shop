e37376f152575f073d89ecc4660f9ef1083496d7
André Abt <info@andre-abt.com>
2018-04-03 10:33:13 +0200

    Testing..

--- src/app/cart/shared/cart.service.spec.ts
+++ src/app/cart/shared/cart.service.spec.ts
@@ -2,16 +2,19 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { CartService } from './cart.service';
 import { MessageService } from '../../messages/message.service';
+import { CartItem } from '../../models/cart-item.model';
+import { Product } from '../../models/product.model';
+import { ToastrService } from 'ngx-toastr';
 
 
-describe('CartService', () => {
+describe('CartService Init', () => {
     beforeEach(() => {
-        const spy = jest.fn();
+        const messageServiceSpy = jest.fn();
 
         TestBed.configureTestingModule({
             providers: [
                 CartService,
-                { provide: MessageService, useValue: spy }
+                { provide: MessageService, useValue: messageServiceSpy }
             ]
         });
     });
@@ -19,4 +22,38 @@ describe('CartService', () => {
     it('should be created', inject([CartService], (service: CartService) => {
         expect(service).toBeTruthy();
     }));
+
+    it('should have init cartItems', inject([CartService], (service: CartService) => {
+        expect(service.getItems()).toEqual([]);
+    }));
+});
+
+
+describe('CartService Methods', () => {
+    beforeEach(() => {
+        const messageServiceMock = jest.mock('../../messages/message.service');
+        messageServiceMock.clearAllMocks();
+
+        TestBed.configureTestingModule({
+            providers: [
+                CartService,
+                { provide: MessageService, useValue: messageServiceMock }
+            ]
+        });
+    });
+
+    it('Add some products', inject([CartService], (service: CartService) => {
+        service.addItem(new CartItem(
+            new Product(
+                666,
+                new Date().toISOString().split('T')[0],
+                'Test Product',
+                'Lorem  Ipsum...',
+                39,
+                39
+            ),
+            3
+        ));
+    }));
+
 });
