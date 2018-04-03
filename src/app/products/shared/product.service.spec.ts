100098ef16d6d8e7640c65968df8cf26dbde3fc6
André Abt <info@andre-abt.com>
2018-04-03 14:47:44 +0200

    Trying to mock AngularFireDatabase

--- src/app/products/shared/product.service.spec.ts
+++ src/app/products/shared/product.service.spec.ts
@@ -7,23 +7,57 @@ import { MessageService } from '../../messages/message.service';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AuthService } from '../../account/shared/auth.service';
 import { FileUploadService } from './file-upload.service';
+import { of } from 'rxjs/observable/of';
+
+class AngularFireDatabaseMock {
+  list(query: string): any {
+    return {
+      valueChanges() {
+        return of([
+          {
+            date: 12345,
+            name: 'Hello World'
+          },
+          {
+            date: 456779,
+            name: 'Hola Mundo'
+          }
+        ]);
+      }
+    };
+  }
+}
 
 describe('ProductService', () => {
+  let productService: ProductService;
+  let angularFireDatabase: AngularFireDatabase;
+
   beforeEach(() => {
+
     TestBed.configureTestingModule({
       providers: [
         ProductService,
         { provide: HttpClient },
         { provide: Router },
         { provide: MessageService },
-        { provide: AngularFireDatabase },
+        { provide: AngularFireDatabase, useClass: AngularFireDatabaseMock },
         { provide: AuthService },
         { provide: FileUploadService },
       ]
     });
+
+    productService = TestBed.get(ProductService);
+    angularFireDatabase = TestBed.get(AngularFireDatabase);
   });
 
-  it('should be created', inject([ProductService], (service: ProductService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created', () => {
+    expect(productService).toBeTruthy();
+  });
+
+
+  it('getProducts()', () => {
+    spyOn(angularFireDatabase, 'list');
+    productService.getProducts();
+    expect(angularFireDatabase.list).toHaveBeenCalled();
+  });
 });
