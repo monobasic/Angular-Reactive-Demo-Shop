9406a5159d46f667ca79c890bd904fa3cb52e1dd
André Abt <info@andre-abt.com>
2018-04-03 09:28:22 +0200

    Added basic jest tests

--- src/app/cart/shared/cart.service.spec.ts
+++ src/app/cart/shared/cart.service.spec.ts
@@ -1,11 +1,18 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { CartService } from './cart.service';
+import { MessageService } from '../../messages/message.service';
+
 
 describe('CartService', () => {
     beforeEach(() => {
+        const spy = jest.fn();
+
         TestBed.configureTestingModule({
-            providers: [CartService]
+            providers: [
+                CartService,
+                { provide: MessageService, useValue: spy }
+            ]
         });
     });
 
--- src/app/products/shared/product.service.spec.ts
+++ src/app/products/shared/product.service.spec.ts
@@ -1,11 +1,32 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ProductService } from './product.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { MessageService } from '../../messages/message.service';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AuthService } from '../../account/shared/auth.service';
+import { FileUploadService } from './file-upload.service';
 
 describe('ProductService', () => {
   beforeEach(() => {
+    const httpSpy = jest.fn();
+    const routerSpy = jest.fn();
+    const messageServiceSpy = jest.fn();
+    const angularFireDatabaseSpy = jest.fn();
+    const authServiceSpy = jest.fn();
+    const uploadServiceSpy = jest.fn();
+
     TestBed.configureTestingModule({
-      providers: [ProductService]
+      providers: [
+        ProductService,
+        { provide: HttpClient, useValue: httpSpy },
+        { provide: Router, useValue: routerSpy },
+        { provide: MessageService, useValue: messageServiceSpy },
+        { provide: AngularFireDatabase, useValue: angularFireDatabaseSpy },
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: FileUploadService, useValue: uploadServiceSpy }
+      ]
     });
   });
 
