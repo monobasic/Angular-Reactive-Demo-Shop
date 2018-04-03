3206a848bdef42b038174c95bc8b88850da6ccad
André Abt <info@andre-abt.com>
2018-04-03 13:00:47 +0200

    Provide services

--- src/app/products/shared/product.service.spec.ts
+++ src/app/products/shared/product.service.spec.ts
@@ -1,11 +1,25 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ProductService } from './product.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { MessageService } from '../../messages/message.service';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AuthService } from '../../account/shared/auth.service';
+import { FileUploadService } from './file-upload.service';
 
 describe('ProductService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ProductService]
+      providers: [
+        ProductService,
+        { provide: HttpClient },
+        { provide: Router },
+        { provide: MessageService },
+        { provide: AngularFireDatabase },
+        { provide: AuthService },
+        { provide: FileUploadService },
+      ]
     });
   });
 
