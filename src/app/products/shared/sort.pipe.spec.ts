27a593e5750999f263b6fe86848ac2ad3c664513
André Abt <info@andre-abt.com>
2018-04-03 08:45:23 +0200

    Deleted unused .spec files

--- src/app/products/shared/sort.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SortPipe } from './sort.pipe';
-
-describe('SortPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SortPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
