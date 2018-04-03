73a9ce2338f65bd36d188b856cf6d781562b83b4
André Abt <info@andre-abt.com>
2018-04-03 10:54:00 +0200

    Added test basics

--- src/app/messages/message.service.spec.ts
+++ src/app/messages/message.service.spec.ts
@@ -1,15 +1,26 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { MessageService } from './message.service';
+import { ToastrService } from 'ngx-toastr';
 
 describe('MessageService Setup', () => {
+
     beforeEach(() => {
+        const toastrServiceSpy = jasmine.createSpyObj('ToastrService', ['success', 'error']);
+
         TestBed.configureTestingModule({
-            providers: [MessageService]
+            providers: [
+                MessageService,
+                { provide: ToastrService, useValue: toastrServiceSpy }
+            ]
         });
     });
 
     it('should be created', inject([MessageService], (service: MessageService) => {
         expect(service).toBeTruthy();
+    }));
+
+    it('init messages array', inject([MessageService], (service: MessageService) => {
+        expect(service.messages).toEqual([]);
     }));
 });
