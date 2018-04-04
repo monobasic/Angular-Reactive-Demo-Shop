3432cdf5c7686dc071611f77b19fcc2272a6b33f
André Abt <info@andre-abt.com>
2018-04-04 17:18:47 +0200

    More testing

--- src/app/cart/shared/cart.service.spec.ts
+++ src/app/cart/shared/cart.service.spec.ts
@@ -117,4 +117,64 @@ describe('CartService Methods', () => {
         expect(messageService.add).toHaveBeenCalledTimes(2);
     });
 
+    it('addItems() 3 products in array at once', () => {
+        const testProduct = new Product(
+            666,
+            new Date().toISOString().split('T')[0],
+            'Foo Product',
+            'Lorem Ipsum...',
+            89,
+            99,
+            10,
+        );
+
+        const testArray = [
+            new CartItem(testProduct, 1),
+            new CartItem(testProduct, 2),
+            new CartItem(testProduct, 3),
+        ];
+
+        spyOn(cartService, 'addItem').and.callThrough();
+
+        // Add a cart item
+        cartService.addItems(testArray);
+
+        // Check items in cart
+        expect(cartService.addItem).toHaveBeenCalledTimes(3);
+        expect(messageService.add).toHaveBeenCalledTimes(3);
+        expect(cartService.getItems()).toEqual([new CartItem(testProduct, 6)]);
+    });
+
+    it('removeItem()', () => {
+        const testProduct = new Product(
+            666,
+            new Date().toISOString().split('T')[0],
+            'Foo Product',
+            'Lorem Ipsum...',
+            89,
+            99,
+            10,
+        );
+
+        spyOn(cartService.itemsChanged, 'emit');
+
+        // Add a cart item
+        cartService.addItem(new CartItem(testProduct, 3));
+
+        // Check items in cart
+        expect(cartService.getItems()).toEqual([
+            new CartItem(testProduct, 3)
+        ]);
+
+        expect(cartService.itemsChanged.emit).toHaveBeenCalled();
+        expect(messageService.add).toHaveBeenCalled();
+
+        // Remove item
+        cartService.removeItem(new CartItem(testProduct, 1));
+
+        expect(cartService.itemsChanged.emit).toHaveBeenCalled();
+        expect(messageService.add).toHaveBeenCalled();
+        expect(cartService.getItems()).toEqual([]);
+    });
+
 });
