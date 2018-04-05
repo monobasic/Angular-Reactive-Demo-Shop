5a30d8f01f7a8c43c4b09023c9b0d3e64461d724
Christian Zellweger <chzellweger@gmail.com>
2018-04-05 23:49:32 +0200

    setup firebase for build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCXVTjTuBUXf-UWlpKfKHjomfPezutmPwI',
+    authDomain: 'cas-fee-shop.firebaseapp.com',
+    databaseURL: 'https://cas-fee-shop.firebaseio.com',
+    projectId: 'cas-fee-shop',
+    storageBucket: 'cas-fee-shop.appspot.com',
+    messagingSenderId: '323643286137'
+  }
 };
