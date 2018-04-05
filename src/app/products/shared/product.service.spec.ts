d04a5e7361410d063e21fbf62c2ba44844050d72
Christian Zellweger <chzellweger@gmail.com>
2018-04-05 21:43:16 +0200

    add test for ratings

--- src/app/products/shared/product.service.spec.ts
+++ src/app/products/shared/product.service.spec.ts
@@ -7,22 +7,23 @@ import { MessageService } from '../../messages/message.service';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AuthService } from '../../account/shared/auth.service';
 import { FileUploadService } from './file-upload.service';
+import { Product } from '../../models/product.model';
 
+class MockAuthService {}
 
 describe('ProductService', () => {
   let productService: ProductService;
   let angularFireDatabase: AngularFireDatabase;
 
   beforeEach(() => {
-
     TestBed.configureTestingModule({
       providers: [
         ProductService,
         { provide: Router },
         { provide: MessageService },
         { provide: AngularFireDatabase },
-        { provide: AuthService },
-        { provide: FileUploadService },
+        { provide: AuthService, useClass: MockAuthService },
+        { provide: FileUploadService }
       ]
     });
 
@@ -32,5 +33,50 @@ describe('ProductService', () => {
 
   it('should be created', () => {
     expect(productService).toBeTruthy();
+  });
+
+  describe('should handle rating actions and', () => {
+    beforeEach(() => {
+      productService.authService.getUserUid = () => '123456789';
+    });
+
+    it('should handle a first rating', () => {
+      const product = new Product();
+
+      const result = productService.constructRating(product, 5);
+      console.log(result);
+      expect(result).toEqual({
+        '/ratings/123456789/': 5,
+        '/currentRating/': 5
+      });
+    });
+
+    it('should handle a new rating from same user', () => {
+      const product = new Product();
+      product.currentRating = 5;
+      product.ratings['123456789'] = 5;
+
+      const result = productService.constructRating(product, 1);
+
+      expect(result).toEqual({
+        '/ratings/123456789/': 1,
+        '/currentRating/': 1
+      });
+    });
+
+    it('should handle a rating from a second user', () => {
+      productService.authService.getUserUid = () => '987654321';
+
+      const product = new Product();
+      product.currentRating = 5;
+      product.ratings = {'123456789': 5};
+
+      const result = productService.constructRating(product, 1);
+      console.log('RESULT', result);
+      expect(result).toEqual({
+        '/currentRating/': 3,
+        '/ratings/987654321/': 1
+      });
+    });
   });
 });
