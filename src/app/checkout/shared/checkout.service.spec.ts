ec26c599e1d8c2385bf78a38775e9ac0cc09aa24
Christian Zellweger <chzellweger@gmail.com>
2018-04-05 18:47:25 +0200

    add more tests for checkoutService

--- src/app/checkout/shared/checkout.service.spec.ts
+++ src/app/checkout/shared/checkout.service.spec.ts
@@ -1,5 +1,7 @@
 import { CheckoutService } from './checkout.service';
-import { CheckoutComponent } from '../checkout.component';
+import { Customer } from '../../models/customer.model';
+import { Product } from '../../models/product.model';
+import { CartItem } from '../../models/cart-item.model';
 
 describe('CheckoutService', () => {
   it('should initialize with active step-number 0', () => {
@@ -8,37 +10,91 @@ describe('CheckoutService', () => {
     expect(comp.activeStep).toBe(0);
   });
 
-  it('should increment steps on nextStep', () => {
-    const comp = new CheckoutService();
-    spyOn(comp.stepChanged, 'emit');
+  describe('should handle steps and', () => {
+    let comp;
+    beforeEach(() => {
+      comp = new CheckoutService();
+      spyOn(comp.stepChanged, 'emit');
+    });
 
-    comp.nextStep();
+    it('should increment steps on nextStep', () => {
+      comp.nextStep();
 
-    expect(comp.activeStep).toBe(1);
-    expect(comp.stepChanged.emit).toHaveBeenCalled();
+      expect(comp.activeStep).toBe(1);
+      expect(comp.stepChanged.emit).toHaveBeenCalled();
+    });
+
+    it('should decrement steps on previousStep', () => {
+      comp.nextStep();
+      comp.nextStep();
+      comp.previousStep();
+
+      expect(comp.activeStep).toBe(1);
+      expect(comp.stepChanged.emit).toHaveBeenCalled();
+    });
+
+    it('should reset steps on reset', () => {
+      comp.nextStep();
+      comp.nextStep();
+      comp.resetSteps();
+
+      expect(comp.activeStep).toBe(0);
+      expect(comp.stepChanged.emit).toHaveBeenCalled();
+    });
   });
 
-  it('should decrement steps on previousStep', () => {
-    const comp = new CheckoutService();
-    spyOn(comp.stepChanged, 'emit');
+  describe('should handle additions and', () => {
+    let comp;
+    beforeEach(() => {
+      comp = new CheckoutService();
+      spyOn(comp.orderInProgressChanged, 'emit');
+    });
 
-    comp.nextStep();
-    comp.nextStep();
-    comp.previousStep();
+    it('should handle a shipping method', () => {
+      comp.setPaymentMethod('pay-pal');
 
-    expect(comp.activeStep).toBe(1);
-    expect(comp.stepChanged.emit).toHaveBeenCalled();
-  });
+      expect(comp.getOrderInProgress().paymentMethod).toBe('pay-pal');
+      expect(comp.orderInProgressChanged.emit).toHaveBeenCalled();
+    });
 
-  it('should reset steps on reset', () => {
-    const comp = new CheckoutService();
-    spyOn(comp.stepChanged, 'emit');
+    it('should handle a payment method', () => {
+      comp.setShippingMethod('air freight');
 
-    comp.nextStep();
-    comp.nextStep();
-    comp.resetSteps();
+      expect(comp.getOrderInProgress().shippingMethod).toBe('air freight');
+      expect(comp.orderInProgressChanged.emit).toHaveBeenCalled();
+    });
 
-    expect(comp.activeStep).toBe(0);
-    expect(comp.stepChanged.emit).toHaveBeenCalled();
+    it('should handle a customer', () => {
+      const testCustomer = new Customer(
+        'Hans',
+        'Meier',
+        '',
+        '',
+        8000,
+        '',
+        '',
+        '',
+        ''
+      );
+
+      comp.setCustomer(testCustomer);
+
+      expect(comp.getOrderInProgress().customer).toEqual(
+        new Customer('Hans', 'Meier', '', '', 8000, '', '', '', '')
+      );
+      expect(comp.orderInProgressChanged.emit).toHaveBeenCalled();
+    });
+
+    it('should handle a cart item', () => {
+      const testProduct = new Product();
+      const testCartItem = new CartItem(testProduct, 10);
+
+      comp.setOrderItems([testCartItem]);
+
+      expect(comp.getOrderInProgress().items).toEqual([
+        new CartItem(testProduct, 10)
+      ]);
+      expect(comp.orderInProgressChanged.emit).toHaveBeenCalled();
+    });
   });
 });
