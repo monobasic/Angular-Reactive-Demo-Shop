8d1e033a8200a6646bdfc3b1af9cf6854347de0f
Christian Zellweger <chzellweger@gmail.com>
2018-04-14 18:08:13 +0200

    add consistent modifierers for content-component

--- src/app/core/content/content.component.ts
+++ src/app/core/content/content.component.ts
@@ -1,21 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { OffcanvasService } from '../shared/offcanvas.service';
+import { Component } from '@angular/core';
 
+import { OffcanvasService } from '../shared/offcanvas.service';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
-export class ContentComponent implements OnInit {
-
-  constructor(private offcanvasService: OffcanvasService) { }
-
-  ngOnInit() {
-  }
+export class ContentComponent {
+  constructor(private offcanvasService: OffcanvasService) {}
 
   onMenuClose(e: Event) {
     this.offcanvasService.closeOffcanvasNavigation();
   }
-
 }
