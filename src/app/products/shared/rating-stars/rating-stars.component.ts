8510b1f949c1cc19bf9cfe748438c0ac0aca2dfb
Christian Zellweger <chzellweger@gmail.com>
2018-04-14 18:13:15 +0200

    add consistent modifierers for rating-stars-component

--- src/app/products/shared/rating-stars/rating-stars.component.ts
+++ src/app/products/shared/rating-stars/rating-stars.component.ts
@@ -1,17 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Rating } from '../../../models/rating.model';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-rating-stars',
   templateUrl: './rating-stars.component.html',
   styleUrls: ['./rating-stars.component.scss']
 })
-export class RatingStarsComponent implements OnInit {
-  @Input()
-  rating: number;
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class RatingStarsComponent {
+  @Input() public rating: number;
 }
