fdaf37a70fca92fd9d1d46a84ad93db977493bde
Christian Zellweger <chzellweger@gmail.com>
2018-04-14 18:10:49 +0200

    add consistent modifierers for promo-service

--- src/app/core/shared/promo.service.ts
+++ src/app/core/shared/promo.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+
 import { Observable } from 'rxjs/Observable';
+
 import { Promo } from '../../models/promo.model';
 
 @Injectable()
 export class PromoService {
-
-  constructor(private angularFireDatabase: AngularFireDatabase) { }
+  constructor(private angularFireDatabase: AngularFireDatabase) {}
 
   getPromos(): Observable<Promo[]> {
-    return this.angularFireDatabase.list<Promo>('promos')
-      .valueChanges();
+    return this.angularFireDatabase.list<Promo>('promos').valueChanges();
   }
-
 }
