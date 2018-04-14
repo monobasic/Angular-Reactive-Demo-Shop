82f1065b77aa2bac5bce06e9961d30a29192aaa3
Christian Zellweger <chzellweger@gmail.com>
2018-04-14 18:13:57 +0200

    add consistent modifiers for products-list-item-component

--- src/app/products/products-list-item/products-list-item.component.ts
+++ src/app/products/products-list-item/products-list-item.component.ts
@@ -1,14 +1,13 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 import { Subscription } from 'rxjs/Subscription';
-import { CartService } from '../../cart/shared/cart.service';
 
 import { AuthService } from '../../account/shared/auth.service';
+import { CartService } from '../../cart/shared/cart.service';
 
 import { CartItem } from '../../models/cart-item.model';
-import { User } from '../../models/user.model';
 import { Product } from '../../models/product.model';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-products-list-item',
@@ -16,26 +15,29 @@ import { Product } from '../../models/product.model';
   styleUrls: ['./products-list-item.component.scss']
 })
 export class ProductsListItemComponent implements OnInit, OnDestroy {
-  userSubscription: Subscription;
-  @Input() product: Product;
-  @Input() displayMode: string;
-  user: User;
-  imageLoading: boolean;
+  private userSubscription: Subscription;
+  @Input() public product: Product;
+  @Input() public displayMode: string;
+  public user: User;
+  public imageLoading: boolean;
 
-  constructor(private cartService: CartService, private authService: AuthService) {}
+  constructor(
+    private cartService: CartService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.imageLoading = true;
-    this.userSubscription = this.authService.user.subscribe(user => {
+    this.userSubscription = this.authService.user.subscribe((user) => {
       this.user = user;
     });
   }
 
-  onAddToCart() {
+  public onAddToCart() {
     this.cartService.addItem(new CartItem(this.product, 1));
   }
 
-  onImageLoad() {
+  public onImageLoad() {
     this.imageLoading = false;
   }
 
