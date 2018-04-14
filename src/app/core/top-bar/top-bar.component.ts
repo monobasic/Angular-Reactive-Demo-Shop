0f96fc3a49027870447b13b538aa07909befadde
Christian Zellweger <chzellweger@gmail.com>
2018-04-14 18:14:40 +0200

    add consistent modifiers for top-bar-component

--- src/app/core/top-bar/top-bar.component.ts
+++ src/app/core/top-bar/top-bar.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss']
 })
-export class TopBarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class TopBarComponent {}
