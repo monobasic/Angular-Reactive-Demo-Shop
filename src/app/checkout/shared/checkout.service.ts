f399e0becef58006ede5c3df202d63e661fc7a0e
Christian Zellweger <chzellweger@gmail.com>
2018-04-14 18:07:50 +0200

    add consistent modifierers for checkout-service

--- src/app/checkout/shared/checkout.service.ts
+++ src/app/checkout/shared/checkout.service.ts
@@ -6,60 +6,56 @@ import { CartItem } from '../../models/cart-item.model';
 @Injectable()
 export class CheckoutService {
   private orderInProgress: Order;
-  orderInProgressChanged: EventEmitter<Order> = new EventEmitter<Order>();
-  stepChanged: EventEmitter<number> = new EventEmitter<number>();
-  activeStep: number;
+  public orderInProgressChanged: EventEmitter<Order> = new EventEmitter<
+    Order
+  >();
+  public stepChanged: EventEmitter<number> = new EventEmitter<number>();
+  public activeStep: number;
 
   constructor() {
     this.orderInProgress = new Order(new Customer());
     this.activeStep = 0;
   }
 
-  gotoStep(number) {
+  public gotoStep(number) {
     this.activeStep = number;
     this.stepChanged.emit(this.activeStep);
-
-    console.log(this.orderInProgress);
   }
 
-  nextStep() {
+  public nextStep() {
     this.activeStep++;
     this.stepChanged.emit(this.activeStep);
-
-    console.log(this.orderInProgress);
   }
 
   previousStep() {
     this.activeStep--;
     this.stepChanged.emit(this.activeStep);
-
-    console.log(this.orderInProgress);
   }
 
-  resetSteps() {
+  public resetSteps() {
     this.activeStep = 0;
   }
 
-  setCustomer(customer: Customer) {
+  public setCustomer(customer: Customer) {
     this.orderInProgress.customer = customer;
     this.orderInProgressChanged.emit(this.orderInProgress);
   }
 
-  setShippingMethod(shippingMethod: string) {
+  public setShippingMethod(shippingMethod: string) {
     this.orderInProgress.shippingMethod = shippingMethod;
     this.orderInProgressChanged.emit(this.orderInProgress);
   }
 
-  setOrderItems(items: CartItem[]) {
+  public setOrderItems(items: CartItem[]) {
     this.orderInProgress.items = items;
     this.orderInProgressChanged.emit(this.orderInProgress);
   }
 
-  getOrderInProgress() {
+  public getOrderInProgress() {
     return this.orderInProgress;
   }
 
-  setPaymentMethod(paymentMethod: string) {
+  public setPaymentMethod(paymentMethod: string) {
     this.orderInProgress.paymentMethod = paymentMethod;
     this.orderInProgressChanged.emit(this.orderInProgress);
   }
