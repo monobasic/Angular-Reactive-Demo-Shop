0263c94c5e01eeece7313ff9b086e69c81a97d90
Christian Zellweger <chzellweger@gmail.com>
2018-04-21 16:59:41 +0200

    reintroduce user-guard

--- src/app/account/shared/user.guard.ts
+++ src/app/account/shared/user.guard.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from './auth.service';
+import { map } from 'rxjs/operators/map';
+import { tap } from 'rxjs/operators/tap';
 
 @Injectable()
 export class UserGuard implements CanActivate {
@@ -14,15 +16,15 @@ export class UserGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
-    // return this.authService.user
-    //   .take(1)
-    //   .map(user => user ? true : false)
-    //   .do(authorized => {
-    //     if (!authorized) {
-    //       console.log('route prevented!');
-    //       this.router.navigate(['/register-login']);
-    //     }
-    //  });
+      return this.authService.user.pipe(
+      map(user => user ? true : false),
+      tap(user => {
+        if (user) {
+          return true;
+        } else {
+          this.router.navigate(['./register-login']);
+        }
+      })
+    );
   }
 }
