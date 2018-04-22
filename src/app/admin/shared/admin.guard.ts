0e1de2df353e87a1e2f0bcdbf26325a83dd94560
Christian Zellweger <chzellweger@gmail.com>
2018-04-22 11:24:50 +0200

    change chain to pipe

--- src/app/admin/shared/admin.guard.ts
+++ src/app/admin/shared/admin.guard.ts
@@ -1,31 +1,34 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../../account/shared/auth.service';
 
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/take';
+import { take } from 'rxjs/operators/take';
+import { map } from 'rxjs/operators/map';
+import { tap } from 'rxjs/operators/tap';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-  public canActivate (
+  public canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-
-    return this.authService.user
-      .take(1)
-      .map(user => user && user.roles.admin ? true : false)
-      .do(authorized => {
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.user.pipe(
+      take(1),
+      map((user) => (user && user.roles.admin ? true : false)),
+      tap((authorized) => {
         if (!authorized) {
           console.log('route prevented!');
           this.router.navigate(['/register-login']);
         }
-      });
+      })
+    );
   }
 }
