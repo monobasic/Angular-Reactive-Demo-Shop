54e4035babb9c59aeaf325ad9b616756e1e7aeaf
Christian Zellweger <chzellweger@gmail.com>
2018-04-22 11:23:02 +0200

    change chain to pipe
    see lines 23 to 36

--- src/app/account/shared/user.guard.ts
+++ src/app/account/shared/user.guard.ts
@@ -1,35 +1,38 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from './auth.service';
 
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/take';
+import { take } from 'rxjs/operators/take';
+import { tap } from 'rxjs/operators/tap';
+import { map } from 'rxjs/operators/map';
 
 @Injectable()
 export class UserGuard implements CanActivate {
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   public canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-
-    return this.authService.user
-      .take(1)
-      .do(user => {
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.user.pipe(
+      take(1),
+      tap((user) => {
         console.log('user from guard: ');
         console.log(user);
-      })
-      .map(user => user ? true : false)
-      .do(authorized => {
+      }),
+      map((user) => (user ? true : false)),
+      tap((authorized) => {
         if (!authorized) {
           console.log('route prevented!');
           this.router.navigate(['/register-login']);
         }
-     });
+      })
+    );
   }
 }
